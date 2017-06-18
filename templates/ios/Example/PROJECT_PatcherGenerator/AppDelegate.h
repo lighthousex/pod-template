30cf0279fc56fb0f6ec87be926077e37989b121b
Zitao Xiong <caoer115@gmail.com>
2017-06-18 16:21:20 -0400

    update

---
+++ templates/ios/Example/PROJECT_PatcherGenerator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PROJECT_PatcherGenerator
+//
+//  Created by Zitao Xiong on 6/18/17.
+//  Copyright © 2017 PROJECT_OWNER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ templates/ios/Example/PROJECT_PatcherGenerator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PROJECT_PatcherGenerator
+//
+//  Created by Zitao Xiong on 6/18/17.
+//  Copyright © 2017 PROJECT_OWNER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
