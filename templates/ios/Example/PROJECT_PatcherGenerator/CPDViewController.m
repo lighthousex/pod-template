1747132c2d49552a8a904f0e1e769e890d285906
Zitao Xiong <caoer115@gmail.com>
2017-06-18 21:37:36 -0400

    🚧

--- templates/ios/Example/PROJECT_PatcherGenerator/CPDViewController.m
+++ templates/ios/Example/PROJECT_PatcherGenerator/CPDViewController.m
@@ -7,24 +7,33 @@
 //
 
 #import "CPDViewController.h"
+@import AceFileGenerator;
 
 @interface CPDViewController ()
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
 @property (weak, nonatomic) IBOutlet UILabel *statusLabel;
-
+@property (nonatomic, strong) LHXFileGenerator *fileGenerator;
 @end
 
 @implementation CPDViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
+    self.fileGenerator = [LHXFileGenerator generatorWithConfiguration:^(AceFileGeneratorConfiguration *config) {
+        config.javascriptUserFilesDirectory = [PROJECT_DIR stringByAppendingString:@"../Ace/Javascript/User/"];
+        config.javascriptGenerateFilesDirectory = [PROJECT_DIR stringByAppendingString:@"../Ace/Javascript/Machine/"];
+        config.objcUserFilesDirectory = [PROJECT_DIR stringByAppendingString:@"../Ace/ObjectiveC/User/"];
+        config.objcGenerateFilesDirectory = [PROJECT_DIR stringByAppendingString:@"../Ace/ObjectiveC/Machine/"];
+        config.ignoredFilePath = [PROJECT_DIR stringByAppendingString:@"../AceFileGenerator/Assets/template/ace-files-ignore"];
+    }];
+    
+    [self.fileGenerator generateFilesForClasses:@[@"UIView", @"UIViewController"]];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
++ (NSBundle *)resourcesBundle {
+    NSBundle *appBuilderBundle = [NSBundle bundleForClass:self];
+    NSString *path  = [appBuilderBundle pathForResource:@"PROJECT" ofType:@"bundle"];
+    return [NSBundle bundleWithPath:path];
 }
 
 @end
