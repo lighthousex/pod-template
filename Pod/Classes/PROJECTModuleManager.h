7a7f7ccdde1bf10ec6b34cec1d0a4109efcf8d92
Zitao Xiong <caoer115@gmail.com>
2017-06-24 02:23:45 -0400

    fix

--- Pod/Classes/PROJECTModuleManager.h
+++ Pod/Classes/PROJECTModuleManager.h
@@ -2,12 +2,10 @@
 //  ${POD_NAME}ModuleManager.h
 //  Pods
 //
-//  Created by Zitao Xiong on 6/20/17.
-//
 //
 
 @import Foundation;
-@import LHXKit;
+@import LHXHowl;
 
 @interface ${POD_NAME}ModuleManager : NSObject<LHXAppModuleRegistrable>
 
